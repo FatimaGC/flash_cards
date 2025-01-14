afa8f7989d31a3e0c657bab256cac838d929448c
Fatima Guzman <fguzman1230@gmail.com>
2025-01-14 10:22:53 -0700

    Update to test structure

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,7 +2,7 @@ require './lib/card'
 require './lib/deck'
 
 RSpec.describe Deck do
-  it 'exists' do
+  it 'is an instance of Deck' do
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -13,16 +13,18 @@ RSpec.describe Deck do
     expect(deck).to be_instance_of(Deck) 
   end
 
-  it 'returns cards' do
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  describe '#initialize' do
+    it 'returns cards' do
+      card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    cards = [card_1, card_2, card_3]
-    deck = Deck.new(cards)
+      cards = [card_1, card_2, card_3]
+      deck = Deck.new(cards)
 
-    expect(deck.cards). to eq (cards)
-  end
+      expect(deck.cards). to eq (cards)
+    end
+  end 
 
   describe '#count' do
     it 'returns the number of cards in the deck' do
@@ -49,5 +51,4 @@ RSpec.describe Deck do
       expect(deck.cards_in_category(:STEM)).to eq ([card_2, card_3])
     end
   end
-
-end
+end 
