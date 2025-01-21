69cd9c633ec418fbdf5cc23931f96c9276e4e8dd
Fatima Guzman <fguzman1230@gmail.com>
2025-01-21 10:14:53 -0700

    feat: create runner file and add cards

---
+++ flashcard_runner.rb
@@ -0,0 +1,4 @@
+card_1 = Card.new("What is 5 + 5?", 10, :STEM)
+card_2 = Card.new("What is Rachel's favorite animal?", "dog", :Turing_staff)
+card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_staff)
+card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Beiber", :Turing_staff)
