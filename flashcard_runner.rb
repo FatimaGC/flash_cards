b8c58376eb786180645696c66565c79b7afdc0e7
Fatima Guzman <fguzman1230@gmail.com>
2025-01-22 17:43:05 -0700

    add further code for looping through cards and printing questions and evaluating guesses

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -9,26 +9,46 @@ def start
   card_1 = Card.new("What is 5 + 5?", 10, :STEM)
   card_2 = Card.new("What is Rachel's favorite animal?", "dog", :Turing_staff)
   card_3 = Card.new("What is Mike's middle name?", "nobody knows", :Turing_staff)
-  card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Beiber", :Turing_staff)
+  card_4 = Card.new("What cardboard cutout lives at Turing?", "Justin Bieber", :Turing_staff)
 
   deck = Deck.new([card_1, card_2, card_3, card_4])
 
   round = Round.new(deck)
 
   #Output first message: "Welcome! You're playing with 4 cards."
-  puts welcome = "Welcome! You're playing with #{round.deck.cards.count} cards." #This needs to be outside of the loop so the number never changes. 
+
+  total_number_of_cards = round.deck.cards.count
+  puts "Welcome! You're playing with #{total_number_of_cards} cards." #This needs to be outside of the loop so the number never changes. 
 
   #Loop through deck.cards
+  index = 1
 
-  #Output & actions that need to repeat per card:
+  while !round.deck.cards.empty? do
+    #Output & actions that need to repeat per card:
+
     #Card position - "This is card number X out of 4."
+    puts "This is card number #{index} out of #{total_number_of_cards}"
+
     #Card question - "Question: "
+    puts "Question: #{round.current_card.question}"
 
     #GET user input (guess)
+    guess = gets.chomp #Need to make sure this handles integers and strings so the comparison is correct.
 
-    #Return Turn Feedback (Whether guess was correct or incorrect).
+    #Call take_turn method which does the following:
+      #Takes in the guess
+      #Creates a new turn which includes the current card info and the guess 
+      #Pushes the turn into the turns array of the round 
+      #Shifts the deck to the next card 
+      #Returns the turn 
 
+    current_turn = round.take_turn(guess)
 
+    #Return Turn Feedback (whether guess was correct or incorrect).
+    puts current_turn.feedback
+
+    index += 1
+  end
 end
 
 start()
@@ -63,26 +83,7 @@ start()
 
 
 
-# def start
-#   puts "Welcome! You're playing with #{round.deck.count} cards."
-#   count = 1
 
-#   while !round.deck.cards.empty? do
-#     puts "This is card number #{count} out of #{round.deck.count}." 
-
-#     puts "Question: #{round.current_card.question}"
-
-#     guess = gets.to_i
-
-#     round.take_turn(guess)
-
-#     #How do I access the feedback?
-    
-#     count += 1
-#   end
-# end
-
-# start()
 
 
 
